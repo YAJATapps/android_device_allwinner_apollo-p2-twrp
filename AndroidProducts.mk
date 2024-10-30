cf10debbadba5c7002c6c61d5370905b09527f13
YAJATapps <yajatapps3@gmail.com>
2024-10-29 22:58:07 -0400

    initial commit (1/2)

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_apollo_p2.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_apollo_p2-user \
+    twrp_apollo_p2-userdebug \
+    twrp_apollo_p2-eng
---
+++ README.md
@@ -0,0 +1,6 @@
+# android_device_allwinner_apollo-p2-twrp
+
+TWRP device tree for Orange Pi Zero 2W / Zero 3
+
+
+
