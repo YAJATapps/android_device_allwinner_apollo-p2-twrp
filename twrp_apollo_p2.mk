73c2b143137d905abdf09b214b5cce8dbacc0c1f
YAJATapps <yajatapps3@gmail.com>
2024-12-06 00:32:32 -0500

    pi0_3 option

--- twrp_apollo_p2.mk
+++ twrp_apollo_p2.mk
@@ -1,3 +1,6 @@
+# Whether the build is for orange pi zero 2w (true) or orange pi zero 3 (false)
+PRODUCT_ORANGE_PI_ZERO_2W := true
+
 # Inherit from products
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -13,5 +16,11 @@ PRODUCT_DEVICE := apollo-p2
 PRODUCT_NAME := twrp_apollo_p2
 PRODUCT_BRAND := Allwinner
 PRODUCT_MANUFACTURER := Allwinner
-PRODUCT_MODEL := orangepizero2w
+ifeq ($(PRODUCT_ORANGE_PI_ZERO_2W), true)
+    PRODUCT_MODEL := orangepizero2w
+    TARGET_PREBUILT_KERNEL := device/allwinner/apollo-p2/kernel
+else
+    PRODUCT_MODEL := orangepizero3
+    TARGET_PREBUILT_KERNEL := device/allwinner/apollo-p2/kernel3
+endif
 
